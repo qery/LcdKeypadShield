cd21646967fe82c8fccdbac5b29f9926265f4bdf
qery <qery@qery.net>
2016-10-20 17:14:03 +0200

    Added files
    First commit

---
+++ LcdKeypadShield/LcdKeypadShield.h
@@ -0,0 +1,63 @@
+#ifndef LCDKEYPADSHIELD_H
+#define LCDKEYPADSHIELD_H
+
+#include <LiquidCrystal.h>
+#include <Arduino.h>
+
+#define KEY_NONE    0
+#define KEY_RIGHT   1
+#define KEY_UP      2
+#define KEY_DOWN    3
+#define KEY_LEFT    4
+#define KEY_SELECT  5
+
+class LcdKeypad : public LiquidCrystal
+{
+  private:
+    int adc_val[5] ={30,150,360,535,760};
+    int adc_value=0;
+
+    unsigned long time_start=0;
+    
+    byte key_current=KEY_NONE;
+    byte key_last=KEY_NONE;
+  public:
+    LcdKeypad() : LiquidCrystal(8,9,4,5,6,7)
+    {
+      pinMode(A0,INPUT);
+      begin(16,2);
+      clear();
+    };
+    byte keyPressed()
+    {
+      adc_value=analogRead(A0);
+
+      for (byte i=0;i<5;i++)
+        if (adc_value<adc_val[i])
+        {
+          if (time_start==0)
+          {
+            time_start=millis();
+            return KEY_NONE;
+          }
+          else
+          {
+            if (millis()-time_start>250)
+            {
+              key_last=key_current;
+              key_current=i+1;
+              if (key_current!=key_last)
+              {
+                time_start=0;
+                return  key_current;
+              }
+              else
+                return KEY_NONE;
+            }
+          }
+        }
+      key_current=KEY_NONE;
+      return KEY_NONE;
+    };
+};
+#endif
---
+++ LcdKeypadShield/examples/KeyTest/KeyTest.ino
@@ -0,0 +1,47 @@
+#include"LcdKeypadShield.h"
+#include<LiquidCrystal.h>
+
+LcdKeypad lcdShield;
+
+void setup()
+{
+  lcdShield.clear();
+  lcdShield.println("LCDKeypad Shield");
+  lcdShield.println("testing keys...");
+}
+
+int keycode=0;
+int count=0;
+
+void loop()
+{
+  keycode=lcdShield.keyPressed();
+  if (keycode>0)
+  {
+    lcdShield.setCursor(0,2);
+    switch (keycode)
+    {
+      case KEY_RIGHT:
+        lcdShield.print("Right  ");
+        count++;
+        break;
+      case KEY_UP:
+        lcdShield.print("Up     ");
+        count++;
+        break;
+      case KEY_DOWN:
+        lcdShield.print("Down   ");
+        count++;
+        break;
+      case KEY_LEFT:
+        lcdShield.print("Left   ");
+        count++;
+        break;
+      case KEY_SELECT:
+        lcdShield.print("Select ");
+        count++;
+        break;
+    }
+    lcdShield.print(count);
+  }
+}
